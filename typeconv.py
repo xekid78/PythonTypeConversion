80d34081e9a90809c6ae690bdbaa443a13c1ab60
Makoto Tanabe <xekid78@outlook.com>
2018-03-06 22:45:08 +0900

    Add files via upload

---
+++ typeconv.py
@@ -0,0 +1,17 @@
+print("** ケース1 **")
+num1 = 3.15
+print(num1)
+print(int(num1))
+print()
+
+print("** ケース２ **")
+num2 = 5
+print(num2)
+print(float(num2))
+print()
+
+print("** ケース３ **")
+text = "123"
+print(text)
+print(int(text) * 100)
+print(float(text))
